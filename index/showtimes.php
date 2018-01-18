fc8df9f3ab3b8412d72d7c61abbbd52a60b9650d
c_kc <lyafret@gmail.com>
2018-01-18 23:21:37 +0100

    19

--- index/showtimes.php
+++ index/showtimes.php
@@ -1,6 +1,6 @@
 
 <?php
 header('Content-Type: application/json');
-$arr = array(array('id' => '456'),array('id' => '123'));
+$arr = array(array('id' => 456),array('id' => 123));
 echo json_encode($arr);
 ?>
