16b851ba5b17c22a010cea2673af2f13837f2b74
c_kc <lyafret@gmail.com>
2018-01-18 23:34:36 +0100

    20

--- index/showtimes.php
+++ index/showtimes.php
@@ -1,6 +1,6 @@
 
 <?php
 header('Content-Type: application/json');
-$arr = array(array('id' => 456),array('id' => 123));
+$arr = array(array('id' => '456','name' => 'Star-Wars'),array('id' => '123','name' => 'Les Minions'));
 echo json_encode($arr);
 ?>
