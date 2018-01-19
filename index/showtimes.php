ec224640b228511be3fac6df9a03432463811fe7
c_kc <lyafret@gmail.com>
2018-01-19 05:06:02 +0100

    4

--- index/showtimes.php
+++ index/showtimes.php
@@ -22,7 +22,11 @@ header('Content-Type: application/json');
 		{
 
 			
-			$data = ['id' => strval(rand(1, 4000)),'name' => $json_obj['name']]; 
+			$data = ['id' => strval(rand(1, 4000)),'name' => $json_obj['name']];
+
+			$sql = "INSERT INTO seances (name)
+			VALUES ($json_obj['name'])";
+			echo $sql;
 			echo json_encode($data);
 		}
 		else
